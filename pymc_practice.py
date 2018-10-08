d2d82bccb849f3b5e9077f6d2e1e90e224e93ceb
Xueying <xueyingwang.x@gmail.com>
2018-10-08 22:45:23 +0800

    pymc

---
+++ pymc_practice.py
@@ -0,0 +1,49 @@
+
+# pymc learning example 
+#reference: https://people.duke.edu/~ccc14/sta-663/PyMC2.html
+
+import os
+import sys
+import glob 
+import matplotlib.pyplot as plt
+import numpy as np
+import panda as pd
+import scipy.stats as stats
+import pymc
+plt.style.use('ggplot')
+
+# coin toss
+
+n = 100
+h = 61
+alpha = 2
+beta = 2
+
+p = pymc.Beta('p',alpha=alpha,beta=beta)
+y = pymc.Binomial('y',n=n,p=p,value=h,observed=True)
+m = pymc.Model([p,y])
+
+mc = pymc.MCMC(m,)
+mc.sample(iter=11000,burn=10000)
+plt.hist(p.trace(), 15, histtype='step', normed=True, label='post');
+x = np.linspace(0, 1, 100)
+plt.plot(x, stats.beta.pdf(x, alpha, beta), label='prior');
+plt.legend(loc='best');
+
+
+# Estimating mean and standard deviation of normal distribution
+# generate observed data
+N = 100
+y = np.random.normal(10, 2, N)
+
+# define priors
+mu = pymc.Uniform('mu', lower=0, upper=100)
+tau = pymc.Uniform('tau', lower=0, upper=1)
+
+# define likelihood
+y_obs = pymc.Normal('Y_obs', mu=mu, tau=tau, value=y, observed=True)
+
+# inference
+m = pymc.Model([mu, tau, y])
+mc = pymc.MCMC(m)
+mc.sample(iter=11000, burn=10000)
