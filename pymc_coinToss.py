6015ac5e3eb0ab0f3d09a07ad8945a8e34c44f88
Xueying <xueyingwang.x@gmail.com>
2018-10-08 22:39:46 +0800

    mcmc

--- pymc_coinToss.py
+++ pymc_coinToss.py
@@ -8,6 +8,8 @@ import glob
 import matplotlib.pyplot as plt
 import numpy as np
 import panda as pd
+import scipy.stats as stats
+import pymc
 plt.style.use('ggplot')
 
 n = 100
@@ -15,4 +17,13 @@ h = 61
 alpha = 2
 beta = 2
 
-p = 
+p = pymc.Beta('p',alpha=alpha,beta=beta)
+y = pymc.Binomial('y',n=n,p=p,value=h,observed=True)
+m = pymc.Model([p,y])
+
+mc = pymc.MCMC(m,)
+mc.sample(iter=11000,burn=10000)
+plt.hist(p.trace(), 15, histtype='step', normed=True, label='post');
+x = np.linspace(0, 1, 100)
+plt.plot(x, stats.beta.pdf(x, alpha, beta), label='prior');
+plt.legend(loc='best');
