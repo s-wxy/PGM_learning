e64d17dcd5235dce3ec97d7f2370a406834e6fc1
Xueying <xueyingwang.x@gmail.com>
2018-10-08 21:31:40 +0800

    pymc

---
+++ pymc_coinToss.py
@@ -0,0 +1,18 @@
+
+# pymc learning example 
+#reference: https://people.duke.edu/~ccc14/sta-663/PyMC2.html
+
+import os
+import sys
+import glob 
+import matplotlib.pyplot as plt
+import numpy as np
+import panda as pd
+plt.style.use('ggplot')
+
+n = 100
+h = 61
+alpha = 2
+beta = 2
+
+p = 
